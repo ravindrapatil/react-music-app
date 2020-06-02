63c282fb2c37e2dae4177023e0da39a8eb64a7c1
Ravindra <ravindra.spatil@gmail.com>
2020-06-02 18:33:56 +0530

    PUBG stats changes...

--- src/components/pubGStats/Dashboard.js
+++ src/components/pubGStats/Dashboard.js
@@ -43,9 +43,10 @@ function Dashboard() {
 
     const [state, setstate] = useState({
         query: '',
+        id: '',
         statusCode: '',
-        playerData: [],
-        gameData: []
+        playerData: {},
+        gameData: {}
     });
     const { query, playerData, gameData, statusCode } = state;
 
@@ -58,7 +59,7 @@ function Dashboard() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        getPlayerInfo(query)
+        getPlayerInfo(query);
     }
 
     const getPlayerInfo = async (query) => {
@@ -73,13 +74,19 @@ function Dashboard() {
                 setstate({
                     ...state,
                     statusCode: apiData.status,
-                    playerData: playerData.concat(query, apiData.data.data[0].id)
+                    playerData: {
+                        query: query,
+                        playerId: apiData.data.data[0].id
+                    }
                 });
             } else {
                 setstate({
                     ...state,
                     statusCode: 404,
-                    playerData: []
+                    playerData: {
+                        query: '',
+                        playerId: ''
+                    }
                 });
             }
         } catch (error) {
@@ -88,36 +95,36 @@ function Dashboard() {
     }
 
     useEffect(() => {
-        if (playerData.length > 0) {
+        if (Object.keys(playerData).length > 0) {
             getPlayerGameData();
         }
     }, [playerData])
 
     const getPlayerGameData = async () => {
-        const playerGameData = await playerGameModeStatistics({
-            request: {
-                firstName: 'ravi',
-                mode: playerData[1]
+        if (playerData.playerId) {
+            const playerGameData = await playerGameModeStatistics({
+                request: {
+                    firstName: 'ravi',
+                    mode: playerData.playerId
+                }
+            });
+            if (playerGameData && playerGameData.data.data.length > 0) {
+                setstate({
+                    ...state,
+                    gameData: {
+                        playerName: playerData.query,
+                        data: playerGameData.data.data[0].attributes.gameModeStats.squad
+                    }
+                })
             }
-        });
-        console.log(playerGameData)
-        if (playerGameData && playerGameData.data.data.length > 0) {
-            setstate({
-                ...state,
-                gameData: gameData.concat(
-                    playerData,
-                    playerGameData.data.data[0].attributes.gameModeStats.squad.kills,
-                    playerGameData.data.data[0].attributes.gameModeStats.squad.damageDealt,
-                    playerGameData.data.data[0].attributes.gameModeStats.squad.headshotKills,
-                    playerGameData.data.data[0].attributes.gameModeStats.squad.longestKill,
-                    playerGameData.data.data[0].attributes.gameModeStats.squad.vehicleDestroys
-                )
-            })
         }
     }
 
     return (
         <div>
+            <Typography variant="h4" style={{ margin: '20px 0 0', textAlign: 'center', color: '#176cd6' }}>
+                Statistics for PUBG-PC version
+            </Typography>
             <form onSubmit={handleSubmit}>
                 <Paper className={classes.root}>
                     <SearchIcon style={{ marginLeft: '6px' }} />
@@ -133,47 +140,70 @@ function Dashboard() {
                     </IconButton>
                 </Paper>
                 <div style={{ textAlign: 'center', margin: '10px 0', fontSize: '12px', color: '#c3c0c0' }}>
-                    eg: king
+                    eg: king, HunterGrewal, Terrific, Cinderella
                 </div>
             </form>
+
+            {/* {
+                gameData && Object.keys(gameData).length &&
+                <p>{gameData.playerName} - {gameData.data.assists}</p>
+            } */}
+
             {
-                statusCode !== 404 && gameData && gameData.length > 0 &&
-                <Grid container spacing={3} style={{ backgroundColor: '#f5f5f5', marginTop: '50px' }}>
-                    <Grid item xs={4} sm={6} md={4} lg={4}>
-                        <Typography variant="h5">
-                            {gameData[0]}
+                statusCode !== 404 && gameData && Object.keys(gameData).length ?
+                    <Grid container spacing={3} style={{ backgroundColor: '#f5f5f5', marginTop: '50px' }}>
+                        <Grid item xs={12} sm={4} md={4} lg={4}>
+                            <Typography variant="h5">
+                                {gameData.playerName}
+                            </Typography>
+                            <Divider light style={{ margin: '20px 0' }} />
+                            <Typography variant="h6">
+                                About
                         </Typography>
-                        <Divider light style={{ margin: '20px 0' }} />
-                        <Typography variant="h6">
-                            About
-                        </Typography>
-                        <Typography variant="body2">
-                            The following attributes are the LIFETIME stats of the player from the PC-Steam version of PlayerUnknown's Battle Grounds.These stas are being fetched from PUBG's developer API. No data is saved on the server. All rights reserved to PUBG and it's products. More data will be avaialble as I am maintaning it..
+                            <Typography variant="body2">
+                                The following attributes are the LIFETIME stats of the player from the PC-Steam version of PlayerUnknown's Battle Grounds.These stas are being fetched from PUBG's developer API. No data is saved on the server. All rights reserved to PUBG and it's products. More data will be avaialble as I am maintaning it..
                     </Typography>
-                    </Grid>
-                    <Grid item xs={8} sm={6} md={4} lg={4}>
-                        <Typography variant="h5">
-                            Stats
+                        </Grid>
+                        <Grid item xs={12} sm={8} md={8} lg={8}>
+                            <Typography variant="h5">
+                                Stats
                         </Typography>
-                        <List>
-                            <ListItem>
-                                Total Kills: {gameData[2]}
-                            </ListItem>
-                            <ListItem>
-                                Damage Dealt: {gameData[3]}
-                            </ListItem>
-                            <ListItem>
-                                Headshot Kills: {gameData[4]}
-                            </ListItem>
-                            <ListItem>
-                                Longest Kill: {gameData[5]}
-                            </ListItem>
-                            <ListItem>
-                                Vehicles Destroyed: {gameData[6]}
-                            </ListItem>
-                        </List>
+                            <Grid container>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    Daily Kills:
+                            </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    {gameData.data.dailyKills}
+                                </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    Damage Dealt:
+                            </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    {gameData.data.damageDealt}
+                                </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    Headshot Kills:
+                            </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    {gameData.data.headshotKills}
+                                </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    Longest Kill:
+                            </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    {gameData.data.longestKill}
+                                </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    Vehicles Destroyed:
+                            </Grid>
+                                <Grid item xs={6} sm={6} md={6} lg={6} className="pubgStats">
+                                    {gameData.data.vehicleDestroys}
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </Grid>
-                </Grid>
+                    :
+                    ''
             }
             {
                 statusCode === 404 && <div style={{ margin: '30px auto 0', textAlign: 'center', fontSize: '13px' }}>
